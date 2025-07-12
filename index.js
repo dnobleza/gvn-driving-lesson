7f9c9106bc8f8b29544d277e4eaf52daa28e680f
myGitHubDenobleza <xceedgamingofficial@gmail.com>
2025-07-12 23:27:19 +0800

    update index

--- index.js
+++ index.js
@@ -4,3 +4,13 @@ const jwt = require('jsonwebtoken');
 const { pool } = require ('pg');
 require ('dotenv').config();
 
+// dotenv.config();
+// connectDB;
+
+const app = express();
+
+app.use(express.json());
+// app.use('/api/auth', require('./routes/authRoutes'));
+
+const PORT = process.env.PORT  || 5000;
+app.listen(PORT, () => console.log(`server is now running on port ${PORT}`));
